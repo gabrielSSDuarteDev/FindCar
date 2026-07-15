4d840fda254d55892ad1596a674d859c9367751a
Gabriel Soares De Souza Duarte <gabrielssduarte.dev@gmail.com>
2026-07-14 21:47:31 -0300

    Create: Alert.java

---
+++ src/main/java/br/com/alura/FindCar/model/Alert.java
@@ -0,0 +1,92 @@
+package br.com.alura.FindCar.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_alerts")
+public class Alert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "fipe_code", nullable = false)
+    private String fipeCode;
+
+    @Column(name = "model_name", nullable = false)
+    private String modelName;
+
+    @Column(name = "target_price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal targetPrice;
+
+    @Column(name = "current_price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal currentPrice;
+
+
+    public Alert(){}
+
+    public Alert(User user, String fipeCode, String modelName, BigDecimal targetPrice, BigDecimal currentPrice) {
+        this.user = user;
+        this.fipeCode = fipeCode;
+        this.currentPrice = currentPrice;
+        this.modelName = modelName;
+        this.targetPrice = targetPrice;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFipeCode() {
+        return fipeCode;
+    }
+
+    public void setFipeCode(String fipeCode) {
+        this.fipeCode = fipeCode;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public BigDecimal getTargetPrice() {
+        return targetPrice;
+    }
+
+    public void setTargetPrice(BigDecimal targetPrice) {
+        this.targetPrice = targetPrice;
+    }
+
+    public BigDecimal getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(BigDecimal currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+}
+
+
+
